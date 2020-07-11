adc76fc621fd21d15a9c8f5e0cdbf993056603a6
porao <ratomorto@riseup.net>
2020-07-10 23:23:14 -0300

    change name + add the code

---
+++ app.js
@@ -0,0 +1,18 @@
+let count = 0;
+const value = document.getElementById('value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (button) {
+    button.addEventListener('click', function(a) {
+        const styles = a.currentTarget.classList;
+        
+        if(styles.contains('decrease')) count--;
+        else if(styles.contains('increase')) count++;
+        else count = 0;
+
+        if(count>0) value.style.color = 'green';
+        else if (count<0) value.style.color = 'red';
+        else value.style.color = '#222'; 
+        value.textContent = count;
+    }) 
+})
